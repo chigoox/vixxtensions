4428874dcc00d6e6d312a627df73069ed57d9369
chigoox <dikeemmanuel54@gmail.com>
2023-08-31 15:22:12 -0400

    up

--- app/Componets/Header/NavBar.jsx
+++ app/Componets/Header/NavBar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <nav className='h-24 bg-black w-full border'>
-            <h1>test</h1>
-        </nav>
+        <div className='h-12 bg-black w-full border'>
+            <nav>
+                <Link>Home</Link>
+            </nav>
+        </div>
 
     )
 }
