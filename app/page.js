033ceac40d7d8293da860717e6885d4bddab86bd
ED <dikeemmanuel54@gmail.com>
2023-09-01 10:06:18 -0400

    DONE WITH NAVBAR

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <h1>ENTERS VIX BEST HAIR STORE!</h1>
-      div
+      
     </main>
   )
 }
