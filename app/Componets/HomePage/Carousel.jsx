9577f845ad68560d7f833d64b8fbbaa11a1c09f3
chigoox <dikeemmanuel54@gmail.com>
2023-09-01 20:40:48 -0400

    corucel added

--- app/Componets/HomePage/Carousel.jsx
+++ app/Componets/HomePage/Carousel.jsx
@@ -1,9 +1,11 @@
 'use client'
 import React, { useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
+import { AiFillBackward, AiFillStepBackward, AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
 
 export const EmblaCarousel = ({ img1, img2, img3 }) => {
-    const [emblaRef, emblaApi] = useEmblaCarousel()
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
 
     const scrollPrev = useCallback(() => {
         if (emblaApi) emblaApi.scrollPrev()
@@ -14,23 +16,25 @@ export const EmblaCarousel = ({ img1, img2, img3 }) => {
     }, [emblaApi])
 
     return (
-        <div className="embla ">
-            <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla relative">
+            <div className="embla__viewport from-slate-400 to-slate-500 bg-gradient-to-br h-[334.89px] overflow-hidden" ref={emblaRef}>
                 <div className="embla__container zoom">
                     {img1 &&
-                        <div className="embla__slide"> <img src={img1} alt="" /></div>}
+                        <div className="embla__slide"> <img className='object-cover' src={img1} alt="" /></div>}
                     {img2 &&
-                        <div className="embla__slide"><img src={img2} alt="" /></div>}
+                        <div className="embla__slide"><img className='object-cover' src={img2} alt="" /></div>}
                     {img3 &&
-                        <div className="embla__slide"><img src={img3} alt="" /></div>}
+                        <div className="embla__slide"><img className='object-cover' src={img3} alt="" /></div>}
                 </div>
             </div>
-            <button className="embla__prev" onClick={scrollPrev}>
-                Prev
-            </button>
-            <button className="embla__next" onClick={scrollNext}>
-                Next
-            </button>
+            <div className='absolute top-[70%] between gap-4 p-4 w-full'>
+                <button className="embla__prev hover:scale-110 trans scale-100 rounded-full border border-dotted bg-white bg-opacity-50" onClick={scrollPrev}>
+                    <AiOutlineArrowLeft size={32} color='white' />
+                </button>
+                <button className="embla__next hover:scale-110 trans scale-100 rounded-full border border-dotted bg-white bg-opacity-50" onClick={scrollNext}>
+                    <AiOutlineArrowRight size={32} color='white' />
+                </button>
+            </div>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -5,7 +5,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <EmblaCarousel img1={'https://Yummyextensions.com/cdn/shop/files/hero-banner-2019-2_420822c0-6107-485b-ba96-551c1b2697a5_1950x.jpg'} />
+      <EmblaCarousel img1={'https://Yummyextensions.com/cdn/shop/files/hero-banner-2019-2_420822c0-6107-485b-ba96-551c1b2697a5_1950x.jpg'}
+      img2={'https://static.wixstatic.com/media/53a3ab_51f8142a5f024605a53a6dede0e5e250~mv2.png/v1/fill/w_1919,h_1080,al_c,q_95,enc_auto/53a3ab_51f8142a5f024605a53a6dede0e5e250~mv2.png'} 
+      img3={'https://static.wixstatic.com/media/53a3ab_3b3d05b47884499cb57590fcf11aa824~mv2.jpg/v1/fill/w_1462,h_971,al_t,q_85,enc_auto/53a3ab_3b3d05b47884499cb57590fcf11aa824~mv2.jpg'}
+      />
       
     </main>
   )
