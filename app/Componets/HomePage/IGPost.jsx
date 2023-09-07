e6394b2d250097d3c6797bf4dfedae79f89afcfe
ED <dikeemmanuel54@gmail.com>
2023-09-07 12:54:13 -0400

    muted video

--- app/Componets/HomePage/IGPost.jsx
+++ app/Componets/HomePage/IGPost.jsx
@@ -7,7 +7,7 @@ function IGPost({ post }) {
 
     return (
         <div className='rounded-[1rem]  shadow-md shadow-gray-700 group bg-black m-auto overflow-hidden  h-36 w-36 '>
-            {mediaType == 'VIDEO' && <video onMouseOver={e => e.target.play()} onMouseOut={e => e.target.pause()} className='video h-full w-full trans object-cover group-hover:scale-110' src={mediaUrl} alt="" />}
+            {mediaType == 'VIDEO' && <video muted="muted" onMouseOver={e => e.target.play()} onMouseOut={e => e.target.pause()} className='video h-full w-full trans object-cover group-hover:scale-110' src={mediaUrl} alt="" />}
             {mediaType == 'IMAGE' && <img className='h-full w-full trans-slow object-cover group-hover:scale-110' src={mediaUrl} alt="" />}
         </div>
     )
