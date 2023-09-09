90f5fdcca224478139fbe093c0f7c6b9cb0e2e17
ED <dikeemmanuel54@gmail.com>
2023-09-09 03:49:51 -0400

    update

--- app/Componets/HomePage/IGFeed.jsx
+++ app/Componets/HomePage/IGFeed.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import IGPost from './IGPost';
 
+async function getData() {
+    const res = await fetch('https://feeds.behold.so/a8MIJ35pXYq2syUql3wg')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
 
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
 
 async function IGFeed() {
-
-    const response = await fetch('https://feeds.behold.so/a8MIJ35pXYq2syUql3wg', { next: { revalidate: 0 } });
-    const jsonFeed = await response.json();
-    const feedMap = jsonFeed.map(instagramPost => (<IGPost post={instagramPost} />))
+    const IGData = await getData()
+    const IGPosts = IGData.map(instagramPost => (<IGPost post={instagramPost} />))
 
 
     return (
         <div className='grid md:grid-cols-3 md:grid-rows-2 grid-cols-2 overflow-hidden  w-full md:w-fit items-center  gap-2 md:[&>*:nth-child(1)]:relative [&>*:nth-child(1)]:left-14 [&>*:nth-child(1)]:top-14 [&>*:nth-child(3)]:right-14 [&>*:nth-child(3)]:h-64 md:[&>*:nth-child(3)]:h-36  md:[&>*:nth-child(3)]:relative [&>*:nth-child(3)]:bottom-6  md:[&>*:nth-child(2)]:h-64 [&>*:nth-child(2)]:h-64 [&>*:nth-child(2)]:w-40  md:[&>*:nth-child(2)]:w-64  [&>*:nth-child(5)]:h-32 md:[&>*:nth-child(5)]:relative [&>*:nth-child(5)]:bottom-16 [&>*:nth-child(5)]:left-14 [&>*:nth-child(5)]:w-32 md:[&>*:nth-child(4)]:relative [&>*:nth-child(4)]:left-40 [&>*:nth-child(4)]:bottom-14 md:[&>*:nth-child(4)]:h-36 md:[&>*:nth-child(4)]:w-36 [&>*:nth-child(4)]:h-36 [&>*:nth-child(4)]:w-36 [&>*:nth-child(1)]: md:[&>*:nth-child(6)]:relative [&>*:nth-child(6)]:h-40 [&>*:nth-child(6)]:w-40 [&>*:nth-child(6)]:bottom-32 [&>*:nth-child(6)]:right-12'>
-            {feedMap.slice(0, 6)}
+            {IGPosts.slice(0, 6)}
         </div>
     )
 }
