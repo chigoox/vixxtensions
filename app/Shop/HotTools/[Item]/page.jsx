4c697ced154bf908118270dc1dfdd22b5e88bb9a
chigoox <dikeemmanuel54@gmail.com>
2023-09-13 23:19:10 -0400

    j

--- app/Shop/HotTools/[Item]/page.jsx
+++ app/Shop/HotTools/[Item]/page.jsx
@@ -3,9 +3,13 @@
 import React from 'react'
 
 import { useNavEvent } from "@/app/Componets/NavigationEvents"
-
-export default function LuxHotToolItemPage({ repo }) {
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
+export default function LuxHotToolItemPage({ params }) {
   const [path] = useNavEvent()
+  const { id } = params
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly">
 
