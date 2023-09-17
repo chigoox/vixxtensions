250578a968939743cc0ba5da231ea2e1185d5db8
chigoox <dikeemmanuel54@gmail.com>
2023-09-17 00:15:00 -0400

    fgh

--- app/Shop/HotTools/[Item]/page.jsx
+++ app/Shop/HotTools/[Item]/page.jsx
@@ -59,16 +59,42 @@ export default function LuxHotToolItemPage({ params }) {
 
           </div>
           <div className='mt-2 '>
-            <div className='border-black border gap-4 items-center h-12 flex md:flex-row flex-col'>
+            <h1 className='text-center font-light'>Quntity</h1>
+            <div className='border-black border gap-4 items-center  flex md:flex-row flex-col'>
               <input type="number" className='w-20 font-bold h-9 p-2 border border-slate-300' />
               <button className='h-12 w-48 bg-gray-500'>ADD TO CART</button>
             </div>
           </div>
+
+          <div className='border mt-2 '>
+            <h1 className='text-2xl font-extralight text-white bg-black-800'>Description</h1>
+            <h1 className='p-2'>
+              Lorem ipsum dolor sit amet,
+              consectetuer adipiscing elit.
+              Aenean commodo ligula eget dolor.
+              Aenean massa. Cum sociis natoque
+              penatibus et magnis dis parturient
+              montes, nascetur ridiculus mus.
+              Donec quam felis, ultricies nec,
+              pellentesque eu, pretium quis,
+              sem. Nulla consequat massa quis
+              enim.
+            </h1>
+            <h1 className='mt-4 p-2'>
+              Donec pede justo, fringilla vel,
+              aliquet nec, vulputate eget,
+              arcu. In enim justo, rhoncus ut,
+              imperdiet a, venenatis vitae, justo.
+              Nullam dictum felis eu pede mollis pretium.
+              Integer tincidunt. Cras dapibus. Vivamus elementum
+              semper nisi. Aenean vulputate eleifend tellus.
+            </h1>
+
+
+          </div>
         </div>
 
       </div>
-
-
     </main>
   )
 }
