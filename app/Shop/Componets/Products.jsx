121118ee4528b003d608f0c821d9539897a1885e
chigoox <dikeemmanuel54@gmail.com>
2023-09-19 03:44:25 -0400

    update

--- app/Shop/Componets/Products.jsx
+++ app/Shop/Componets/Products.jsx
@@ -12,7 +12,7 @@ export const Products = async ({ category }) => {
             <div className='grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  w-full m-auto'>
                 {prices.data.map(product => {
                     return (
-                        <ShopItem key={product} shopItems={product} />
+                        <ShopItem key={product} location={category} shopItems={product} />
 
                     )
                 })}
