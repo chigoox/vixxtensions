dc6ccff80bbca01042935ee5d3530fa2b1870f94
chigoox <dikeemmanuel54@gmail.com>
2023-10-22 22:39:58 -0400

    update

--- app/Shop/page.jsx
+++ app/Shop/page.jsx
@@ -1,29 +1,54 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductsList from './Componets/ProductsList'
 import Link from 'next/link'
 import { category } from '../META'
+import { fetchProducts } from '../myCodes/Stripe'
 
 const ShopSections = ({ category, name }) => {
     return (
         <div className='text-3xl overflow-x-scroll '>
-            <Link href={`/Shop/${category}`} className='center relative font-extralight top-12 underline'>{name}</Link>
+            <Link href={`/Shop/${category}`} className='center relative font-extralight top-12 underline'>
+                {name}
+            </Link>
             <ProductsList category={category} limit={4} list />
         </div>
     )
 
 }
 
+const getData = async (set) => {
+    category.forEach(async (category) => {
+        const data = await fetchProducts(category.replace(/\s/g, ''))
+        if (data[0]) set(o => ([...o, category]))
+
+    });
+}
+
+
 
 function Shop() {
+    const [categoryWithProducts, setCategoryWithProducts] = useState([])
+
+    const filteredCategory = [...new Set(categoryWithProducts)]
+
+    console.log(filteredCategory)
+
+    useEffect(() => {
+        (async () => {
+            await getData(setCategoryWithProducts)
+        })()
+
+    }, [])
+
 
 
     return (
         <div className='flex min-h-screen flex-col '>
-            {category.map(category => {
+            {filteredCategory.map(category => {
                 return (
 
-                    <ShopSections category={category.replace(/\s/g, '')} name={category} />
+                    <ShopSections key={category} category={category.replace(/\s/g, '')} name={category} />
                 )
             })}
 
